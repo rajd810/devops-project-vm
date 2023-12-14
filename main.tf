4b71c205305797738c3c98bb0d02c37afb684c1b
Raj Dhumal <rajd810@outlook.com>
2023-12-14 13:45:52 +0530

    Testing Commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-#DECLARING THE AWS INSTANNCE TYPE
+#DECLARING THE AWS INSTANNCE TYPE - Testing Git commit
 resource "aws_instance" "web" {
     ami                         = "ami-0287a05f0ef0e9d9a"
     instance_type               = "t2.micro"
